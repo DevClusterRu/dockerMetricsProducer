dd004fcfd5b9496ee48cbd91ef1056603caa03a7
Den <weblifeon@gmail.com>
2022-08-08 21:23:18 +0300

    last

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/mackerelio/go-osstat/memory"
 	"log"
 	"math"
+	"net"
 	"net/rpc"
 	"os"
 	"time"
@@ -19,16 +20,69 @@ type System struct {
 
 var hostname string
 
+func isPrivateIP(ip net.IP) bool {
+	var privateIPBlocks []*net.IPNet
+	for _, cidr := range []string{
+		// don't check loopback ips
+		//"127.0.0.0/8",    // IPv4 loopback
+		//"::1/128",        // IPv6 loopback
+		//"fe80::/10",      // IPv6 link-local
+		"10.0.0.0/8",     // RFC1918
+		"172.16.0.0/12",  // RFC1918
+		"192.168.0.0/16", // RFC1918
+	} {
+		_, block, _ := net.ParseCIDR(cidr)
+		privateIPBlocks = append(privateIPBlocks, block)
+	}
+
+	for _, block := range privateIPBlocks {
+		if block.Contains(ip) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func LocalIP() (net.IP, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			return nil, err
+		}
+
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+
+			if isPrivateIP(ip) {
+				return ip, nil
+			}
+		}
+	}
+
+	return nil, err
+}
+
 func main() {
-	var err error
-	hostname, err = os.Hostname()
+
+	ip, err := LocalIP()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for {
 		sys := System{
-			Node: hostname,
+			Node: ip.String(),
 		}
 
 		memory, err := memory.Get()
