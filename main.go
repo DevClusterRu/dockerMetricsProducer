73e0e8fd1973ac915cadd8b6a532ca8405221b40
Den <weblifeon@gmail.com>
2022-08-09 12:23:53 +0300

    last

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func isPrivateIP(ip net.IP) bool {
 	var privateIPBlocks []*net.IPNet
 	for _, cidr := range []string{
 		// don't check loopback ips
+
 		//"127.0.0.0/8",    // IPv4 loopback
 		//"::1/128",        // IPv6 loopback
 		//"fe80::/10",      // IPv6 link-local
