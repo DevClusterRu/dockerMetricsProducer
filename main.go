c4c1294c0d678b60470a9aa02d3e8ba2a02b9fe2
Den <weblifeon@gmail.com>
2022-08-09 12:57:56 +0300

    last

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"net/rpc"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -19,31 +20,6 @@ type System struct {
 }
 
 var hostname string
-
-func isPrivateIP(ip net.IP) bool {
-	var privateIPBlocks []*net.IPNet
-	for _, cidr := range []string{
-		// don't check loopback ips
-
-		//"127.0.0.0/8",    // IPv4 loopback
-		//"::1/128",        // IPv6 loopback
-		//"fe80::/10",      // IPv6 link-local
-		//"10.0.0.0/8",     // RFC1918
-		"172.0.0.0/8", // RFC1918
-		//"192.168.0.0/16", // RFC1918
-	} {
-		_, block, _ := net.ParseCIDR(cidr)
-		privateIPBlocks = append(privateIPBlocks, block)
-	}
-
-	for _, block := range privateIPBlocks {
-		if block.Contains(ip) {
-			return true
-		}
-	}
-
-	return false
-}
 
 func LocalIP() (net.IP, error) {
 	ifaces, err := net.Interfaces()
@@ -65,7 +41,7 @@ func LocalIP() (net.IP, error) {
 				ip = v.IP
 			}
 
-			if isPrivateIP(ip) {
+			if strings.Contains(ip.String(), "172.") {
 				return ip, nil
 			}
 		}
