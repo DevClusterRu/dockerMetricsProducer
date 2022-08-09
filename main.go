a1228de7b418fc9e55df26bf40fd141f474fa4e1
Den <weblifeon@gmail.com>
2022-08-09 12:43:40 +0300

    last

--- main.go
+++ main.go
@@ -28,9 +28,9 @@ func isPrivateIP(ip net.IP) bool {
 		//"127.0.0.0/8",    // IPv4 loopback
 		//"::1/128",        // IPv6 loopback
 		//"fe80::/10",      // IPv6 link-local
-		"10.0.0.0/8",     // RFC1918
-		"172.16.0.0/12",  // RFC1918
-		"192.168.0.0/16", // RFC1918
+		//"10.0.0.0/8",     // RFC1918
+		"172.0.0.0/8", // RFC1918
+		//"192.168.0.0/16", // RFC1918
 	} {
 		_, block, _ := net.ParseCIDR(cidr)
 		privateIPBlocks = append(privateIPBlocks, block)
